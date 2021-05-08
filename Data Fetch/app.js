4f44981b2eb9474dbe21cabf0b6a7f298f8fb6c3
Anupam-USP <ianupam1509@gmail.com>
2021-05-09 01:05:35 +0530

    Final patch

--- Data Fetch/app.js
+++ Data Fetch/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+// const datepicker = require("js-datepicker");
 
-const {
-  MongoClient
-} = require("mongodb");
+const app = express();
+
+app.use(express.urlencoded({ extented: true }));
+
+const { MongoClient } = require("mongodb");
 // Replace the uri string with your MongoDB deployment's connection string.
 const url =
   "mongodb+srv://anupamAdmin:anupamAdmin@cluster0.2c2zj.mongodb.net/student_db";
@@ -14,31 +16,34 @@ const client = new MongoClient(url, {
 });
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("student.ejs");
 });
 
 app.get("/records", (req, res) => {
-
   const database = client.db("student_db");
   const students = database.collection("student_records");
   students.find({}).toArray(function (err, std_record) {
     // console.log(std_record);
     res.render("records", {
-      records: std_record
+      records: std_record,
     });
   });
 });
 
 app.get("/Student", function (req, res) {
-  res.redirect("/")
-})
+  res.redirect("/");
+});
 
 app.get("/Admin", function (req, res) {
   res.render("Admin.ejs");
-})
+});
+
+app.get("/cal", function (req, res) {
+  res.render("cal.ejs");
+});
 
 app.get("/stdTable", (req, res) => {
   // res.render("stdTable");
@@ -47,12 +52,42 @@ app.get("/stdTable", (req, res) => {
   students.find({}).toArray(function (err, std_record) {
     // console.log(std_record);
     res.render("stdTable", {
-      records: std_record
+      records: std_record,
     });
   });
 });
 
+app.post("/", function (req, res) {
+  let roll = req.body.rollno;
+  const database = client.db("student_db");
+  const students = database.collection("student_records");
+  students.find({}).toArray(function (err, std_record) {
+    res.render("stdTable", {
+      records: std_record,
+      rollno: roll,
+      basis: 1,
+    });
+  });
+});
 
+app.post("/cal", function (req, res) {
+  console.log(req.body);
+  // let name = req.body.stdName;
+  let date = req.body.date;
+  const database = client.db("student_db");
+  const students = database.collection("student_records");
+  students.find({}).toArray(function (err, std_record) {
+    res.render("stdTable", {
+      records: std_record,
+      date: date,
+      basis: 2,
+    });
+  });
+});
+
+// app.get("/Student", function (req, res) {
+//   res.redirect("/")
+// });
 
 function fetchData() {
   const database = client.db("student_db");
@@ -74,7 +109,11 @@ async function run() {
 }
 run().catch(console.dir);
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(port, () => {
-  console.log('App listening at http://localhost:' + port);
+  console.log("App listening at http://localhost:" + port);
 });
